d9aee93579306a819b513556f75710972b651652
cendalesv <cendalescaceres@hotmail.com>
2024-11-26 00:00:29 -0500

    act codigo

--- router/auth.js
+++ router/auth.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import singIn from "../conntrollers/auth/signIn.js";
 import userNotExist from "../middlewares/userNotExist.js";
+import isValidPassword from "../middlewares/isVaildPassword.js";
 
 const routerAuth = Router()
 
-routerAuth.post('/signin', userNotExist, singIn)
+routerAuth.post('/signin', userNotExist, isValidPassword, singIn)
 
 export default routerAuth
